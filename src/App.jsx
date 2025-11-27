2fbe9f666bc6156c575bc9537a6c094ca0f5cd2d
Yakov Zainullin <yayaza2006@bk.ru>
2025-11-27 14:15:27 +0300

    fix link

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function App() {
   };
 
 return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <div className="grid-background"></div>
       <div className="content">
         <nav>
